194ecb0fa2785ee63f2081df65e5bf96c0c4e964
Anaol Atinafu <atinafuanaol@gmail.com>
2024-05-24 10:42:50 +0300

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import javax.swing.JOptionPane;
+
+public class sum {
+
+    sum() {
+
+        double num1, num2;
+
+        String n1 = JOptionPane.showInputDialog(null, "Enter first integer");
+        String n2 = JOptionPane.showInputDialog(null, "Enter second integer");
+
+        num1 = Double.parseDouble(n1);
+        num2 = Double.parseDouble(n2);
+
+        double sum = num1 + num2;
+        JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum of Two Integers",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        new sum();
+    }
+}
