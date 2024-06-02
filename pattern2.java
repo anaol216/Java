06aec350680b6052313ba817752049da991d6b61
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-02 22:46:27 +0300

    Update pattern2.java
    inverted triangle

--- pattern2.java
+++ pattern2.java
@@ -2,8 +2,8 @@ public class Main{
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <=5; j++) {
-                if(j>5-i){
-                     System.out.print(" *");
+                if(j>=i){
+                     System.out.print("* ");
                 }else {
                     System.out.print(" ");
                 }     
@@ -11,4 +11,5 @@ public class Main{
             System.out.println();
         }
     }
+}
 }
