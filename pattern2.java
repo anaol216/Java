f668c673749de8932a265f722dd72b211e75a4d8
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-02 22:35:24 +0300

    Update pattern2.java
    triangle with astrix

--- pattern2.java
+++ pattern2.java
@@ -1,8 +1,12 @@
-public class pattern2 {
+public class Main{
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 6 - i; j++) {
-                System.out.print("*");
+            for (int j = 1; j <=5; j++) {
+                if(j>5-i){
+                     System.out.print(" *");
+                }else {
+                    System.out.print(" ");
+                }     
             }
             System.out.println();
         }
