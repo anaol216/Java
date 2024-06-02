7c7a43063143350dd7cf7109290552aaf85025bf
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-02 22:59:48 +0300

    Update pattern2.java
    Diamond with  four loops

--- pattern2.java
+++ pattern2.java
@@ -1,5 +1,15 @@
 public class Main{
     public static void main(String[] args) {
+        for (int i = 1; i <4; i++) {
+            for (int j = 1; j <=4; j++) {
+                if(j>=4-i){
+                     System.out.print(" *");
+                }else {
+                    System.out.print(" ");
+                }     
+            }
+            System.out.println();
+        }
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <=5; j++) {
                 if(j>=i){
@@ -11,5 +21,4 @@ public class Main{
             System.out.println();
         }
     }
-}
 }
