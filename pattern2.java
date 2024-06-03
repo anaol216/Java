6ad6337ab8b97c3cb20ed47f3db33d0a95572d12
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-03 22:08:57 +0300

    Update pattern2.java
    floyd's triangle

--- pattern2.java
+++ pattern2.java
@@ -1,22 +1,11 @@
-public class Main{
+
+public class pattern1 {
     public static void main(String[] args) {
-        for (int i = 1; i <4; i++) {
-            for (int j = 1; j <=4; j++) {
-                if(j>=4-i){
-                     System.out.print(" *");
-                }else {
-                    System.out.print(" ");
-                }     
-            }
-            System.out.println();
-        }
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <=5; j++) {
-                if(j>=i){
-                     System.out.print("* ");
-                }else {
-                    System.out.print(" ");
-                }     
+        int a = 1;
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a + " ");
+                a++;
             }
             System.out.println();
         }
