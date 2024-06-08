6bbd20cab29d4ca6d2a7f3a03fcfa8c1d916fae5
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-08 22:54:04 +0300

    zigzag pattern out

--- pattern2.java
+++ pattern2.java
@@ -1,13 +1,24 @@
-
-public class pattern1 {
+public class pattern2 {
     public static void main(String[] args) {
-        int a = 1;
-        for (int i = 1; i < 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(a + " ");
-                a++;
+        int count;
+        for (int i = 0; i <= 4; i++) {
+            if (i % 2 == 0) {
+                count = (5 * (i)) + 1;
+                for (int j = 0; j <= 4; j++) {
+                    System.out.print(count + " ");
+                    count++;
+                }
+            }
+            if (i % 2 != 0) {
+                count = (5 * (i + 1));
+                for (int j = 0; j <= 4; j++) {
+                    System.out.print(count + " ");
+                    count--;
+                }
             }
             System.out.println();
+
         }
     }
 }
+
