debfec72f171a7e1fcedcea7a1d8ee76ce2fad56
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-09 23:25:17 +0300

    Update pattern1.java
    v print

---
+++ pattern1.java
@@ -0,0 +1,16 @@
+public class pattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 9; j++) {
+                if (j == i || j == (10 - i))
+                    System.out.print(j + " ");
+                else
+                    System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}
