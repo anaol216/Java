26a27942943d4839a382d22c62155b60a489db62
Anaol Atinafu <atinafuanaol@gmail.com>
2024-06-10 23:15:02 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,4 @@
-Certainly! Here's a Java implementation of a recursive function to calculate the Fibonacci sequence using a0 as 6 and a1 as 4:
 
-```java
 public class Fibonacci {
     public static int fibonacci(int n) {
         if (n == 0) {
@@ -18,14 +16,3 @@ public class Fibonacci {
         System.out.println("The " + n + "th Fibonacci number is: " + result);
     }
 }
-```
-
-In this code, we define a class called `Fibonacci` with a static method `fibonacci` that takes an input `n` and returns the `n`th Fibonacci number. The base cases are set to return 6 for `n = 0` and 4 for `n = 1`. For any other value of `n`, the function recursively calls itself with `n-1` and `n-2` as inputs and returns the sum of the two previous Fibonacci numbers.
-
-In the `main` method, we calculate the 9th Fibonacci number using `n = 9` and store the result in the `result` variable. Finally, we print the result using `System.out.println()`.
-
----
-Learn more:
-1. [recursion - Java recursive Fibonacci sequence - Stack Overflow](https://stackoverflow.com/questions/8965006/java-recursive-fibonacci-sequence)
-2. [Print Fibonacci sequence using 2 variables - GeeksforGeeks](https://www.geeksforgeeks.org/print-fibonacci-sequence-using-2-variables/)
-3. [Fibonacci Series in Java Using Recursion - Scaler Topics](https://www.scaler.com/topics/fibonacci-series-in-java-using-recursion/)
